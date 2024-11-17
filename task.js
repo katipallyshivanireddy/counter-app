dcb97fdefe0161916cc82c48caba21be79a46260
ranjith reddy <katipallyshivanireddy1@gmail.com>
2024-11-17 18:38:06 +0530

    counter app

---
+++ task.js
@@ -0,0 +1,29 @@
+
+// let countValue = document.getElementById("count");
+// let inc = document.getElementById("in")
+// let dec = document.getElementById("de")
+// let reset = document.getElementById("reset")
+
+
+// reset.addEventListener("click", () => {
+
+
+//     count = 0
+//     display()
+// })
+
+// function display() {
+//     countValue.textContent = count;
+// }
+
+// let count = 0
+
+// dec.addEventListener("click", function () {
+//     count=count--
+//     display()
+
+// })
+// inc.addEventListener("click", function () {
+//     count=count++;
+//     display()
+// })
